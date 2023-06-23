dfb9e18345e5d01e1c2f157ae05600564470b579
Yolkkreate <yolkkreate.dev@gmail.com>
2023-06-24 00:21:51 +0700

    Adding example on Step bar type

--- example/lib/pages/category_page.dart
+++ example/lib/pages/category_page.dart
@@ -1,12 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:polar_tab_bar/models/polar_tab_item.dart';
+import 'package:polar_tab_bar/polar_tab_bar.dart';
+import 'package:polar_tab_bar/widgets/polar_tab_page.dart';
 
 class CategoryPage extends StatelessWidget {
   const CategoryPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Category"),
+    final List<PolarTabItem> tabs = [
+      PolarTabItem(
+        index: 0,
+        title: "All",
+        page: const PolarTabPage(
+          child: Center(child: Text("All")),
+        ),
+      ),
+      PolarTabItem(
+        index: 1,
+        title: "Active",
+        page: const PolarTabPage(
+          child: Center(child: Text("Active")),
+        ),
+      ),
+      PolarTabItem(
+        index: 2,
+        title: "Inactive",
+        page: const PolarTabPage(
+          child: Center(child: Text("Inactive")),
+        ),
+      ),
+    ];
+
+    return PolarTabBar(
+      type: PolarTabType.step,
+      tabs: tabs,
+      isDense: true,
     );
   }
 }
